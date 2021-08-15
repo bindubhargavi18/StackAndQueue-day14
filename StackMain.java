189d4e156d5aed9b8271589aaae6f6c5c80f7ac1
bindubhargavi568 <bindubhargavi568@gmail.com>
2021-08-15 15:47:05 +0530

    Added:welcome stack using linkedlist

---
+++ StackMain.java
@@ -0,0 +1,10 @@
+package com.stack.linkedlist;
+
+public class StackMain 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("welcome to stack implementation using linked list");
+	}
+
+}
